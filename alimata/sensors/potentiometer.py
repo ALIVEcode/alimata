fecdb8b06f93c05a7030e0e505bb002dfc3fb327
Nilon123456789 <51246940+Nilon123456789@users.noreply.github.com>
2022-11-05 14:42:48 -0400

    ADD - added option to return mapped value for the potentiometer class

--- alimata/sensors/potentiometer.py
+++ alimata/sensors/potentiometer.py
@@ -1,4 +1,4 @@
-from alimata.core.core import PIN_MODE
+from alimata.core.core import PIN_MODE, maprange
 from alimata.sensors.sensor import Sensor
 from alimata.core.board import Board
 from typing import Callable, Union, List
@@ -12,8 +12,11 @@ class Potentiometer(Sensor):
     ----------
     pin : str
         analog pin of the Potentiometer
-    invert : bool
-        if the Potentiometer is inverted or not
+    
+    Methods
+    ---------
+    mapped_data(min, max) : int
+        Return the current value of the Potentiometer mapped to the given range (min to max)
 
     Properties
     ----------
@@ -23,25 +26,24 @@ class Potentiometer(Sensor):
 
 
 
-    def __init__(self, board: Board, pin: str, invert: bool = False, differential: int = 1, on_change: Union[Callable[[List[Union[float, int]]], None], None ]= None):
+    def __init__(self, board: Board, pin: str, differential: int = 1, on_change: Union[Callable[[List[Union[float, int]]], None], None ]= None):
         
 
         # Initialises the Potentiometer as 0
         self.__data = 0 # PRIVATE
 
-        # Initialises the invert value
-        self.invert = invert # PUBLIC
-
         super().__init__(board=board, pin=pin, type_=PIN_MODE.ANALOG_INPUT, differential=differential, on_change=on_change)
 
 
+    def mapped_data(self, min: int, max: int) -> int:
+        """Return the current value of the Potentiometer mapped to the given range (min to max)"""
+        return maprange(self.__data, 0, 255, min, max)
 
     # ABSTRACT FROM SENSOR
     @property
     def data(self) -> bool:
         """Return the current value of the Potentiometer (True or False)"""
         return self.__data
-
 
     # ABSTRACT FROM SENSOR
     # Change the value of the Potentiometer
