654dc28599dbfde728903a65cc91df0a6b488ae5
Nilon123456789 <dowers.purer0q@icloud.com>
2022-11-23 08:53:41 -0500

    MOD - updated firmetix requirement

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ if __name__ == "__main__":
               include=['alimata', 'alimata.*']),
           include_package_data=True,
           python_requires=">=3.7",
-          install_requires=["firmetix>=1.0.0"],
+          install_requires=["firmetix>=6.0.1"],
           setup_requires="setuptools",
           )
